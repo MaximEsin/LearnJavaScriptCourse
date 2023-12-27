fe34ca4b8ea46602ea01118060a04f9a799e324e
Max <maximesin21@gmail.com>
2023-12-27 15:39:01 +0700

    3.5 task done

---
+++ Part 3/3.5/index.js
@@ -0,0 +1,10 @@
+// Accept: */*
+// Accept-Charset: utf-8
+// Accept-Encoding: gzip,deflate,sdch
+// Connection: keep-alive
+// Host: google.com
+// Origin: http://javascript.info
+// Referer: http://javascript.info/some/url
+
+// Origin is needed here, because the request is cross-origin
+// Referer is not necessary
--- Part 3/Readme.md
+++ Part 3/Readme.md
@@ -5,3 +5,5 @@
 2.1 ArrayBuffer
 
 3.1 Fetch
+
+3.5 Fetch: crossorigin
